9f326f00ce8da4879d988458970b7f0e41a9fda1
Felipe Forte <felipeforte@protonmail.com>
2024-11-08 17:39:11 -0300

    get

--- src/bet/core.clj
+++ src/bet/core.clj
@@ -1,9 +1,16 @@
 (ns bet.core
   (:gen-class))
 
+(require '[clj-http.client :as client])
+(require '[cheshire.core :as json])
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
-
-(+ 1 5)
+  (println "Buscando resposta GET...")
+  (def resp (client/get "https://betano.p.rapidapi.com/events" {:headers {:x-rapidapi-key "4a99664d09msh4338b9e97205a19p14bf36jsne665166aa327"
+                                                                          :x-rapidapi-host "betano.p.rapidapi.com"}
+                                                                :query-params {:tournamentId "38"}})
+  )
+  (println resp)
+)
