1af1f27d5daf4c359e0fbca1797e3230b408cd25
Felipe Forte <felipeforte@protonmail.com>
2024-11-10 09:34:34 -0300

    adicionando primeiro função pra capturar json

--- src/bet/core.clj
+++ src/bet/core.clj
@@ -1,16 +1,22 @@
 (ns bet.core
   (:gen-class))
 
-(require '[clj-http.client :as client])
-(require '[cheshire.core :as json])
+(require '[clj-http.client :as client]
+         '[cheshire.core :as json])
+
+(defn get-json-sports
+  "Retorna JSON com lista de esportes disponíveis"
+  []
+  ;; (println "Buscando resposta GET...")
+  ;; (def resp (client/get "https://betano.p.rapidapi.com/sports" {:headers {:x-rapidapi-key "4a99664d09msh4338b9e97205a19p14bf36jsne665166aa327"
+  ;;                                                                           :x-rapidapi-host "betano.p.rapidapi.com"}}))
+  ;; (spit "test/sports" (:body resp)) ; Salva a chamada da API pra evitar estourar o limite durante a fase de testes
+  ;; (json/parse-string (:body resp) true)  
+  (println (json/parse-string (slurp "test/sports") true))
+  )
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Buscando resposta GET...")
-  (def resp (client/get "https://betano.p.rapidapi.com/events" {:headers {:x-rapidapi-key "4a99664d09msh4338b9e97205a19p14bf36jsne665166aa327"
-                                                                          :x-rapidapi-host "betano.p.rapidapi.com"}
-                                                                :query-params {:tournamentId "38"}})
+  (get-json-sports)
   )
-  (println resp)
-)
